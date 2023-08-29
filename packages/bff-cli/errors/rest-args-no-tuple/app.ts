00f2aa75807bc43e78f6689b0f3c7b8e7e408690
Lucas Avila <lucaxx@gmail.com>
2023-08-28 21:35:37 -0300

    fix broken test

--- packages/bff-cli/errors/rest-args-no-tuple/app.ts
+++ packages/bff-cli/errors/rest-args-no-tuple/app.ts
@@ -1,17 +1,15 @@
-import { GET, todo } from "bff";
-
 type A<X> = [X, X];
 namespace Y {
   export type B = [string, string];
 }
 export default {
-  [`/user`]: async (...q: string[]): Promise<string> => {
+  [`GET/user`]: async (...q: string[]): Promise<string> => {
     return todo();
   },
-  [`/user1`]: async (...q: A<string>): Promise<string> => {
+  [`GET/user1`]: async (...q: A<string>): Promise<string> => {
     return todo();
   },
-  [`/user2`]: async (...q: Y.B): Promise<string> => {
+  [`GET/user2`]: async (...q: Y.B): Promise<string> => {
     return todo();
   },
 };
