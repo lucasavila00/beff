89d4e4e40d86f52d8c39517d26272e5f79004a59
Lucas Avila <lucaxx@gmail.com>
2023-08-30 00:14:04 -0300

    preparing futures

--- packages/bff-wasm/cli/commandeer.ts
+++ packages/bff-wasm/cli/commandeer.ts
@@ -7,17 +7,33 @@ const readProjectJson = (projectPath: string) => {
   return projectJson;
 };
 
+const execProject = (absoluteProjectPath: string) => {
+  const projectJson = readProjectJson(absoluteProjectPath);
+  const router = projectJson.router;
+  const absoluteEntryPoint = path.join(
+    path.dirname(absoluteProjectPath),
+    router
+  );
+
+  const bundler = new Bundler();
+  const outString = bundler.bundle(absoluteEntryPoint);
+
+  const absoluteOutDir = path.join(
+    path.dirname(absoluteProjectPath),
+    projectJson.outputDir
+  );
+  const outputFile = `${absoluteOutDir}/index.js`;
+  fs.writeFileSync(outputFile, outString);
+};
+
+const getAbsolutePathOfProject = (p: string) => {
+  return p;
+};
+
 export const commanderExec = () => {
   program.requiredOption("-p, --project <string>");
   program.parse();
   const options = program.opts();
-  const projectPath = options.project;
-  const projectJson = readProjectJson(projectPath);
-  const router = projectJson.router;
-  const bundler = new Bundler();
-  const entryPoint = path.join(path.dirname(projectPath), router);
-  const outString = bundler.bundle(entryPoint);
-  const outputDir = path.join(path.dirname(projectPath), projectJson.outputDir);
-  const outputFile = `${outputDir}/index.js`;
-  fs.writeFileSync(outputFile, outString);
+  const absoluteProjectPath = getAbsolutePathOfProject(options.project);
+  return execProject(absoluteProjectPath);
 };
