6abbe420282742762cd8e629dbb87d919e9342d3
Lucas Avila <lucaxx@gmail.com>
2023-09-02 23:03:52 -0300

    renames

---
+++ packages/beff-wasm/src/parse_file.rs
@@ -0,0 +1,39 @@
+use beff_core::BffFileName;
+use beff_core::ImportResolver;
+use std::collections::HashMap;
+
+pub struct WasmImportsResolver {
+    resolutions_cache: HashMap<(BffFileName, String), Option<BffFileName>>,
+    current_file: BffFileName,
+}
+
+impl WasmImportsResolver {
+    pub fn new(current_file: BffFileName) -> WasmImportsResolver {
+        WasmImportsResolver {
+            resolutions_cache: HashMap::new(),
+            current_file,
+        }
+    }
+}
+
+impl ImportResolver for WasmImportsResolver {
+    fn resolve_import(&mut self, module_specifier: &str) -> Option<BffFileName> {
+        let module_specifier = module_specifier.to_owned();
+        match self
+            .resolutions_cache
+            .get(&(self.current_file.clone(), module_specifier.clone()))
+        {
+            Some(it) => it.clone(),
+            None => {
+                let v = crate::resolve_import(
+                    self.current_file.to_string().as_str(),
+                    &module_specifier,
+                )
+                .map(BffFileName::new);
+                self.resolutions_cache
+                    .insert((self.current_file.clone(), module_specifier), v.clone());
+                v
+            }
+        }
+    }
+}
