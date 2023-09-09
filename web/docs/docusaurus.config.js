fab0d439b0046a4e05318356b9f0420825b3054a
Lucas Avila <lucaxx@gmail.com>
2023-09-09 20:52:13 -0300

    depoloy to gh pages

--- web/docs/docusaurus.config.js
+++ web/docs/docusaurus.config.js
@@ -11,15 +11,17 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://lucasavila00.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/beff/",
+
+  deploymentBranch: "gh-pages",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "lucasavila00", // Usually your GitHub org/user name.
+  projectName: "beff", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
