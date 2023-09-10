d73534092ce3f513025e59cebf4de8cf9bbf5a21
Lucas Avila <lucaxx@gmail.com>
2023-09-09 21:16:28 -0300

    fix playground

--- web-demo/demo/vite.config.ts
+++ web-demo/demo/vite.config.ts
@@ -6,5 +6,5 @@ import topLevelAwait from "vite-plugin-top-level-await";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
-  base: "/playground/",
+  base: "/beff/playground/",
 });
