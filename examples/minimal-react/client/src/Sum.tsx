69c897c0140cd356783ac51b1193f33e6cda77f8
Lucas Avila <lucaxx@gmail.com>
2023-09-10 16:42:57 -0300

    improve react example

--- examples/minimal-react/client/src/Sum.tsx
+++ examples/minimal-react/client/src/Sum.tsx
@@ -4,19 +4,26 @@ import { beff } from "./utils/beff";
 export function Sum() {
   const [a, setA] = React.useState(1);
   const [b, setB] = React.useState(2);
-  const result = beff["/sum"].get(a, b).useQuery();
+  const queryResult = beff["/sum"].get(a, b).useQuery();
 
   return (
-    <div>
-      Try setting an invalid value and check devtools
+    <div
+      style={{
+        marginTop: 40,
+      }}
+    >
+      <p>
+        <b>Calculator</b>
+      </p>
+      <p>Try setting an invalid value and check devtools</p>
       <div>
         A: <input value={a} onChange={(e) => setA(e.target.value as any)} />
       </div>
       <div>
         B: <input value={b} onChange={(e) => setB(e.target.value as any)} />
       </div>
-      <p>A+B:{result.data?.result}</p>
-      <p>Status: {result.status}</p>
+      <p>A+B:{queryResult.data?.result}</p>
+      <p>Status: {queryResult.status}</p>
     </div>
   );
 }
