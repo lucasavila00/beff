ed48f8b0add64fa3dbd66d75a7968445830bf1e1
Lucas Ávila <lucaxx@gmail.com>
2023-09-21 21:07:59 -0300

    fix ci

--- packages/beff-wasm/vitest/define-function/router.ts
+++ packages/beff-wasm/vitest/define-function/router.ts
@@ -1,9 +1,12 @@
-const r = {
+const r0 = {
   [`/hello1`]: {
     get: async (): Promise<string> => {
       return "Hello!";
     },
   },
+};
+const r = {
+  ...r0,
   [`/hello2`]: {
     get: async function (): Promise<string> {
       return "Hello!";
