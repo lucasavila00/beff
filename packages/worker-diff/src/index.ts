210be3d6ab19a7e37f1c4d482cfb663660f9b416
Lucas Ávila <lucaxx@gmail.com>
2023-09-23 23:50:16 +0000

    worker scaffold

--- packages/worker-diff/src/index.ts
+++ packages/worker-diff/src/index.ts
@@ -1,32 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Client } from 'pg';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	// This should be a valid Postgres connection string
+	// For example, "postgres://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk:5432/pfmegrnargs"
+	// Use `wrangler secret put DB_URL` to configure a Secret with your connection string
+	DB_URL: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		if (url.pathname === '/favicon.ico') return new Response(null, { status: 404 });
+
+		var client = new Client(env.DB_URL);
+		await client.connect();
+
+		// Query the RNA database!
+		const result = await client.query({
+			text: 'SELECT * FROM "Project" LIMIT 10',
+		});
+		console.log(JSON.stringify(result.rows));
+		const resp = Response.json(result.rows);
+
+		// Clean up the client, ensuring we don't kill the worker before that is
+		// completed.
+		ctx.waitUntil(client.end());
+		return resp;
 	},
 };
