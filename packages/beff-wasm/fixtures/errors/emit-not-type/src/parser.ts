8dcc23e240f0bfacd171b643caa79b0fff734928
Lucas Ávila <lucaxx@gmail.com>
2024-05-02 17:03:15 -0300

    prettier

--- packages/beff-wasm/fixtures/errors/emit-not-type/src/parser.ts
+++ packages/beff-wasm/fixtures/errors/emit-not-type/src/parser.ts
@@ -1,9 +1,9 @@
 import parse from "./generated/parser";
 type Shape =
-| { kind: "circle"; radius: number }
-| { kind: "square"; x: number }
-| { kind: "triangle"; x: number; y: number };
+  | { kind: "circle"; radius: number }
+  | { kind: "square"; x: number }
+  | { kind: "triangle"; x: number; y: number };
 
-type T3 = Exclude<Shape, { kind: "circle" }>
+type T3 = Exclude<Shape, { kind: "circle" }>;
 
-export const Codecs = parse.buildParsers<{ T3:T3 }>();
+export const Codecs = parse.buildParsers<{ T3: T3 }>();
