93d3cd481bcb37f5f48c8f047541550762fbd296
Lucas Ávila <lucaxx@gmail.com>
2024-05-03 20:23:04 -0300

    fix it

---
+++ examples/bug-rust-repro/src/generated/parser.d.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+import { DecodeError, StringFormat } from "@beff/cli";
+
+import { ZodType } from "zod";
+
+export type BeffParser<T> = {
+  parse: (input: any) => T;
+  safeParse: (input: any) => { success: true; data: T } | { success: false; errors: DecodeError[] };
+  zod: () => ZodType<T>;
+};
+type Parsers<T> = {
+  [K in keyof T]: BeffParser<T[K]>;
+};
+
+export type TagOfFormat<T extends StringFormat<string>> = T extends StringFormat<infer Tag> ? Tag : never;
+
+export type BuildParserFunction = <T>(args?: {
+  customFormats?: { [key: string]: (input: string) => boolean };
+}) => Parsers<T>;
+
+declare const _exports: {
+  buildParsers: BuildParserFunction
+};
+
+export default _exports;
