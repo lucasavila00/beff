f58a9e3172240a1c2cde09e572b182249055a4fa
Lucas Ávila <lucaxx@gmail.com>
2025-11-20 22:47:01 -0300

    wip

--- e2e-tests/codegen-tests/tests/parser.test.ts
+++ e2e-tests/codegen-tests/tests/parser.test.ts
@@ -20,5 +20,7 @@ it("works", () => {
   expect(Codecs.AliasToConst.describe()).toMatchInlineSnapshot(
     '"type CodecAliasToConst = \\"constant value\\";"',
   );
-  expect(Codecs.TestHoist.describe()).toMatchInlineSnapshot('"type CodecTestHoist = { a: Array<string>, b: Array<string> };"');
+  expect(Codecs.TestHoist.describe()).toMatchInlineSnapshot(
+    '"type CodecTestHoist = { a: Array<string>, b: Array<string> };"',
+  );
 });
