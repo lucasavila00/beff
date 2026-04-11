ce5bfae95f6d1931b0edf8426f40fe4917e2e13a
Lucas Ávila <lucaxx@gmail.com>
2026-04-11 10:30:41 -0300

    Add new string and number formats with custom error messages for parent-child relationships

--- e2e-tests/custom-formats/src/generated/parser.js
+++ e2e-tests/custom-formats/src/generated/parser.js
@@ -66,60 +66,148 @@ const buildParsers = (args) => {
   return acc;
 };
 
-const RequiredStringFormats = ["ValidCurrency","ShortCode","UserId","ReadAuthorizedUserId","WriteAuthorizedUserId"];
-const RequiredNumberFormats = ["NonInfiniteNumber","NegativeNumber","NonNegativeNumber","Rate"];
+const RequiredStringFormats = ["ValidCurrency","ShortCode","UserId","ReadAuthorizedUserId","WriteAuthorizedUserId","StringParentNoMsg","StringChildNoMsg","StringParentMsgOnly","StringParentMsgOnlyChild","StringChildMsgParent","StringChildMsg","StringBothMsgParent","StringBothMsgChild"];
+const RequiredNumberFormats = ["NonInfiniteNumber","NegativeNumber","NonNegativeNumber","Rate","NumberParentNoMsg","NumberChildNoMsg","NumberParentMsgOnly","NumberParentMsgOnlyChild","NumberChildMsgParent","NumberChildMsg","NumberBothMsgParent","NumberBothMsgChild"];
 const direct_hoist_0 = new RefRuntype("ValidCurrency");
 const direct_hoist_1 = new RefRuntype("ShortCode");
 const direct_hoist_2 = new RefRuntype("UserId");
 const direct_hoist_3 = new RefRuntype("ReadAuthorizedUserId");
 const direct_hoist_4 = new RefRuntype("WriteAuthorizedUserId");
-const direct_hoist_5 = new RefRuntype("NonInfiniteNumber");
-const direct_hoist_6 = new RefRuntype("NegativeNumber");
-const direct_hoist_7 = new RefRuntype("NonNegativeNumber");
-const direct_hoist_8 = new RefRuntype("Rate");
-const direct_hoist_9 = new NumberWithFormatRuntype([
+const direct_hoist_5 = new RefRuntype("StringParentNoMsg");
+const direct_hoist_6 = new RefRuntype("StringChildNoMsg");
+const direct_hoist_7 = new RefRuntype("StringParentMsgOnly");
+const direct_hoist_8 = new RefRuntype("StringParentMsgOnlyChild");
+const direct_hoist_9 = new RefRuntype("StringChildMsgParent");
+const direct_hoist_10 = new RefRuntype("StringChildMsg");
+const direct_hoist_11 = new RefRuntype("StringBothMsgParent");
+const direct_hoist_12 = new RefRuntype("StringBothMsgChild");
+const direct_hoist_13 = new RefRuntype("NonInfiniteNumber");
+const direct_hoist_14 = new RefRuntype("NegativeNumber");
+const direct_hoist_15 = new RefRuntype("NonNegativeNumber");
+const direct_hoist_16 = new RefRuntype("Rate");
+const direct_hoist_17 = new RefRuntype("NumberParentNoMsg");
+const direct_hoist_18 = new RefRuntype("NumberChildNoMsg");
+const direct_hoist_19 = new RefRuntype("NumberParentMsgOnly");
+const direct_hoist_20 = new RefRuntype("NumberParentMsgOnlyChild");
+const direct_hoist_21 = new RefRuntype("NumberChildMsgParent");
+const direct_hoist_22 = new RefRuntype("NumberChildMsg");
+const direct_hoist_23 = new RefRuntype("NumberBothMsgParent");
+const direct_hoist_24 = new RefRuntype("NumberBothMsgChild");
+const direct_hoist_25 = new NumberWithFormatRuntype([
     "NegativeNumber"
 ]);
-const direct_hoist_10 = new NumberWithFormatRuntype([
+const direct_hoist_26 = new NumberWithFormatRuntype([
     "NonInfiniteNumber"
 ]);
-const direct_hoist_11 = new NumberWithFormatRuntype([
+const direct_hoist_27 = new NumberWithFormatRuntype([
     "NonInfiniteNumber",
     "NonNegativeNumber"
 ]);
-const direct_hoist_12 = new NumberWithFormatRuntype([
+const direct_hoist_28 = new NumberWithFormatRuntype([
+    "NumberBothMsgParent",
+    "NumberBothMsgChild"
+]);
+const direct_hoist_29 = new NumberWithFormatRuntype([
+    "NumberBothMsgParent"
+]);
+const direct_hoist_30 = new NumberWithFormatRuntype([
+    "NumberChildMsgParent",
+    "NumberChildMsg"
+]);
+const direct_hoist_31 = new NumberWithFormatRuntype([
+    "NumberChildMsgParent"
+]);
+const direct_hoist_32 = new NumberWithFormatRuntype([
+    "NumberParentNoMsg",
+    "NumberChildNoMsg"
+]);
+const direct_hoist_33 = new NumberWithFormatRuntype([
+    "NumberParentMsgOnly"
+]);
+const direct_hoist_34 = new NumberWithFormatRuntype([
+    "NumberParentMsgOnly",
+    "NumberParentMsgOnlyChild"
+]);
+const direct_hoist_35 = new NumberWithFormatRuntype([
+    "NumberParentNoMsg"
+]);
+const direct_hoist_36 = new NumberWithFormatRuntype([
     "NonInfiniteNumber",
     "NonNegativeNumber",
     "Rate"
 ]);
-const direct_hoist_13 = new StringWithFormatRuntype([
+const direct_hoist_37 = new StringWithFormatRuntype([
     "UserId",
     "ReadAuthorizedUserId"
 ]);
-const direct_hoist_14 = new StringWithFormatRuntype([
+const direct_hoist_38 = new StringWithFormatRuntype([
     "ShortCode"
 ]);
-const direct_hoist_15 = new StringWithFormatRuntype([
+const direct_hoist_39 = new StringWithFormatRuntype([
+    "StringBothMsgParent",
+    "StringBothMsgChild"
+]);
+const direct_hoist_40 = new StringWithFormatRuntype([
+    "StringBothMsgParent"
+]);
+const direct_hoist_41 = new StringWithFormatRuntype([
+    "StringChildMsgParent",
+    "StringChildMsg"
+]);
+const direct_hoist_42 = new StringWithFormatRuntype([
+    "StringChildMsgParent"
+]);
+const direct_hoist_43 = new StringWithFormatRuntype([
+    "StringParentNoMsg",
+    "StringChildNoMsg"
+]);
+const direct_hoist_44 = new StringWithFormatRuntype([
+    "StringParentMsgOnly"
+]);
+const direct_hoist_45 = new StringWithFormatRuntype([
+    "StringParentMsgOnly",
+    "StringParentMsgOnlyChild"
+]);
+const direct_hoist_46 = new StringWithFormatRuntype([
+    "StringParentNoMsg"
+]);
+const direct_hoist_47 = new StringWithFormatRuntype([
     "UserId"
 ]);
-const direct_hoist_16 = new StringWithFormatRuntype([
+const direct_hoist_48 = new StringWithFormatRuntype([
     "ValidCurrency"
 ]);
-const direct_hoist_17 = new StringWithFormatRuntype([
+const direct_hoist_49 = new StringWithFormatRuntype([
     "UserId",
     "ReadAuthorizedUserId",
     "WriteAuthorizedUserId"
 ]);
 const namedRuntypes = {
-    "NegativeNumber": direct_hoist_9,
-    "NonInfiniteNumber": direct_hoist_10,
-    "NonNegativeNumber": direct_hoist_11,
-    "Rate": direct_hoist_12,
-    "ReadAuthorizedUserId": direct_hoist_13,
-    "ShortCode": direct_hoist_14,
-    "UserId": direct_hoist_15,
-    "ValidCurrency": direct_hoist_16,
-    "WriteAuthorizedUserId": direct_hoist_17
+    "NegativeNumber": direct_hoist_25,
+    "NonInfiniteNumber": direct_hoist_26,
+    "NonNegativeNumber": direct_hoist_27,
+    "NumberBothMsgChild": direct_hoist_28,
+    "NumberBothMsgParent": direct_hoist_29,
+    "NumberChildMsg": direct_hoist_30,
+    "NumberChildMsgParent": direct_hoist_31,
+    "NumberChildNoMsg": direct_hoist_32,
+    "NumberParentMsgOnly": direct_hoist_33,
+    "NumberParentMsgOnlyChild": direct_hoist_34,
+    "NumberParentNoMsg": direct_hoist_35,
+    "Rate": direct_hoist_36,
+    "ReadAuthorizedUserId": direct_hoist_37,
+    "ShortCode": direct_hoist_38,
+    "StringBothMsgChild": direct_hoist_39,
+    "StringBothMsgParent": direct_hoist_40,
+    "StringChildMsg": direct_hoist_41,
+    "StringChildMsgParent": direct_hoist_42,
+    "StringChildNoMsg": direct_hoist_43,
+    "StringParentMsgOnly": direct_hoist_44,
+    "StringParentMsgOnlyChild": direct_hoist_45,
+    "StringParentNoMsg": direct_hoist_46,
+    "UserId": direct_hoist_47,
+    "ValidCurrency": direct_hoist_48,
+    "WriteAuthorizedUserId": direct_hoist_49
 };
 const buildParsersInput = {
     "ValidCurrency": direct_hoist_0,
@@ -127,10 +215,26 @@ const buildParsersInput = {
     "UserId": direct_hoist_2,
     "ReadAuthorizedUserId": direct_hoist_3,
     "WriteAuthorizedUserId": direct_hoist_4,
-    "NonInfiniteNumber": direct_hoist_5,
-    "NegativeNumber": direct_hoist_6,
-    "NonNegativeNumber": direct_hoist_7,
-    "Rate": direct_hoist_8
+    "StringParentNoMsg": direct_hoist_5,
+    "StringChildNoMsg": direct_hoist_6,
+    "StringParentMsgOnly": direct_hoist_7,
+    "StringParentMsgOnlyChild": direct_hoist_8,
+    "StringChildMsgParent": direct_hoist_9,
+    "StringChildMsg": direct_hoist_10,
+    "StringBothMsgParent": direct_hoist_11,
+    "StringBothMsgChild": direct_hoist_12,
+    "NonInfiniteNumber": direct_hoist_13,
+    "NegativeNumber": direct_hoist_14,
+    "NonNegativeNumber": direct_hoist_15,
+    "Rate": direct_hoist_16,
+    "NumberParentNoMsg": direct_hoist_17,
+    "NumberChildNoMsg": direct_hoist_18,
+    "NumberParentMsgOnly": direct_hoist_19,
+    "NumberParentMsgOnlyChild": direct_hoist_20,
+    "NumberChildMsgParent": direct_hoist_21,
+    "NumberChildMsg": direct_hoist_22,
+    "NumberBothMsgParent": direct_hoist_23,
+    "NumberBothMsgChild": direct_hoist_24
 };
 
 export default { buildParsers };
--- e2e-tests/custom-formats/src/parser.ts
+++ e2e-tests/custom-formats/src/parser.ts
@@ -7,11 +7,27 @@ export type ShortCode = StringFormat<"ShortCode">;
 export type UserId = StringFormat<"UserId">;
 export type ReadAuthorizedUserId = StringFormatExtends<UserId, "ReadAuthorizedUserId">;
 export type WriteAuthorizedUserId = StringFormatExtends<ReadAuthorizedUserId, "WriteAuthorizedUserId">;
+export type StringParentNoMsg = StringFormat<"StringParentNoMsg">;
+export type StringChildNoMsg = StringFormatExtends<StringParentNoMsg, "StringChildNoMsg">;
+export type StringParentMsgOnly = StringFormat<"StringParentMsgOnly">;
+export type StringParentMsgOnlyChild = StringFormatExtends<StringParentMsgOnly, "StringParentMsgOnlyChild">;
+export type StringChildMsgParent = StringFormat<"StringChildMsgParent">;
+export type StringChildMsg = StringFormatExtends<StringChildMsgParent, "StringChildMsg">;
+export type StringBothMsgParent = StringFormat<"StringBothMsgParent">;
+export type StringBothMsgChild = StringFormatExtends<StringBothMsgParent, "StringBothMsgChild">;
 
 export type NonInfiniteNumber = NumberFormat<"NonInfiniteNumber">;
 export type NegativeNumber = NumberFormat<"NegativeNumber">;
 export type NonNegativeNumber = NumberFormatExtends<NonInfiniteNumber, "NonNegativeNumber">;
 export type Rate = NumberFormatExtends<NonNegativeNumber, "Rate">;
+export type NumberParentNoMsg = NumberFormat<"NumberParentNoMsg">;
+export type NumberChildNoMsg = NumberFormatExtends<NumberParentNoMsg, "NumberChildNoMsg">;
+export type NumberParentMsgOnly = NumberFormat<"NumberParentMsgOnly">;
+export type NumberParentMsgOnlyChild = NumberFormatExtends<NumberParentMsgOnly, "NumberParentMsgOnlyChild">;
+export type NumberChildMsgParent = NumberFormat<"NumberChildMsgParent">;
+export type NumberChildMsg = NumberFormatExtends<NumberChildMsgParent, "NumberChildMsg">;
+export type NumberBothMsgParent = NumberFormat<"NumberBothMsgParent">;
+export type NumberBothMsgChild = NumberFormatExtends<NumberBothMsgParent, "NumberBothMsgChild">;
 
 export const Codecs = parse.buildParsers<{
   ValidCurrency: ValidCurrency;
@@ -19,10 +35,26 @@ export const Codecs = parse.buildParsers<{
   UserId: UserId;
   ReadAuthorizedUserId: ReadAuthorizedUserId;
   WriteAuthorizedUserId: WriteAuthorizedUserId;
+  StringParentNoMsg: StringParentNoMsg;
+  StringChildNoMsg: StringChildNoMsg;
+  StringParentMsgOnly: StringParentMsgOnly;
+  StringParentMsgOnlyChild: StringParentMsgOnlyChild;
+  StringChildMsgParent: StringChildMsgParent;
+  StringChildMsg: StringChildMsg;
+  StringBothMsgParent: StringBothMsgParent;
+  StringBothMsgChild: StringBothMsgChild;
   NonInfiniteNumber: NonInfiniteNumber;
   NegativeNumber: NegativeNumber;
   NonNegativeNumber: NonNegativeNumber;
   Rate: Rate;
+  NumberParentNoMsg: NumberParentNoMsg;
+  NumberChildNoMsg: NumberChildNoMsg;
+  NumberParentMsgOnly: NumberParentMsgOnly;
+  NumberParentMsgOnlyChild: NumberParentMsgOnlyChild;
+  NumberChildMsgParent: NumberChildMsgParent;
+  NumberChildMsg: NumberChildMsg;
+  NumberBothMsgParent: NumberBothMsgParent;
+  NumberBothMsgChild: NumberBothMsgChild;
 }>({
   stringFormats: {
     ValidCurrency: {
@@ -41,6 +73,30 @@ export const Codecs = parse.buildParsers<{
       validator: (input: string) => input.includes("_write_"),
       errorMessage: () => "expected user with write permissions",
     },
+    StringParentNoMsg: (input: string) => input.startsWith("string-parent-"),
+    StringChildNoMsg: {
+      validator: (input: string) => input.endsWith("-child"),
+    },
+    StringParentMsgOnly: {
+      validator: (input: string) => input.startsWith("string-parent-msg-"),
+      errorMessage: () => "expected parent string rule",
+    },
+    StringParentMsgOnlyChild: {
+      validator: (input: string) => input.endsWith("-child"),
+    },
+    StringChildMsgParent: (input: string) => input.startsWith("string-child-msg-"),
+    StringChildMsg: {
+      validator: (input: string) => input.endsWith("-child"),
+      errorMessage: () => "expected child string rule",
+    },
+    StringBothMsgParent: {
+      validator: (input: string) => input.startsWith("string-both-msg-"),
+      errorMessage: () => "expected parent string rule",
+    },
+    StringBothMsgChild: {
+      validator: (input: string) => input.endsWith("-child"),
+      errorMessage: () => "expected child string rule",
+    },
   },
   numberFormats: {
     NonInfiniteNumber: {
@@ -57,6 +113,30 @@ export const Codecs = parse.buildParsers<{
     Rate: {
       validator: (input: number) => input >= 0 && input <= 1,
       errorMessage: () => "expected a valid rate",
+    },
+    NumberParentNoMsg: (input: number) => input > 0,
+    NumberChildNoMsg: {
+      validator: (input: number) => input < 10,
+    },
+    NumberParentMsgOnly: {
+      validator: (input: number) => input > 0,
+      errorMessage: () => "expected parent number rule",
+    },
+    NumberParentMsgOnlyChild: {
+      validator: (input: number) => input < 10,
+    },
+    NumberChildMsgParent: (input: number) => input > 0,
+    NumberChildMsg: {
+      validator: (input: number) => input < 10,
+      errorMessage: () => "expected child number rule",
+    },
+    NumberBothMsgParent: {
+      validator: (input: number) => input > 0,
+      errorMessage: () => "expected parent number rule",
+    },
+    NumberBothMsgChild: {
+      validator: (input: number) => input < 10,
+      errorMessage: () => "expected child number rule",
     },
   },
 });
